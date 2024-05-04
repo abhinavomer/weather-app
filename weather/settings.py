f0f8536cde490df8ebd9c6040507c5685609317d
Abhinav Omer <111991497+abhinavomer@users.noreply.github.com>
2024-05-04 12:44:37 +0530

    update media directory

--- weather/settings.py
+++ weather/settings.py
@@ -121,11 +121,11 @@ STATIC_URL='/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_DIR={
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'public/static')
 }
 
-MEDIA_ROOT=os.path.join(BASE_DIR,'static')
-MEDIA_URL='/images/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'public/static')
+MEDIA_URL='/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
